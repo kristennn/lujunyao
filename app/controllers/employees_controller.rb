3b23e194dd8fe0b8fe565247df4b7c9c1f7fd1a3
13949836783@163.com <13949836783@163.com>
2019-02-28 16:35:34 +0800

    完成人员新增功能

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -8,6 +8,22 @@ class EmployeesController < ApplicationController
     @employee = Employee.find(params[:id])
   end
 
+  def new
+    @employee = Employee.new
+  end
+
+  def create
+    employee = Employee.find_by(name: params[:name])
+    if employee.present?
+      flash[:alert] = "人员的姓名不能重复，您填写的姓名已存在"
+    else
+      @employee = Employee.new(employee_params)
+      @employee.save
+      flash[:notice] = "新增成功"
+    end
+    redirect_to employees_path
+  end
+
   def import_employee
     if !params[:file].present?
       flash[:alert] = "您还没有选择文件哦"
@@ -22,4 +38,11 @@ class EmployeesController < ApplicationController
     end
     redirect_to employees_path
   end
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(:name, :sex, :job_number, :worktype, :department, :duty, :birth_date, :working_date)
+  end
+
 end
