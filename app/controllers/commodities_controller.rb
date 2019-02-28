f8fa7de1ca34dfd661747c186de892b3fb6c1ab2
13949836783@163.com <13949836783@163.com>
2019-02-28 16:46:17 +0800

    完成商品新增功能

--- app/controllers/commodities_controller.rb
+++ app/controllers/commodities_controller.rb
@@ -13,6 +13,18 @@ class CommoditiesController < ApplicationController
     @commodity = Commodity.find(params[:id])
   end
 
+  def create
+    commodity = Commodity.find_by(name: params[:name])
+    if commodity.present?
+      flash[:alert] = "商品名称不能重复，您填写的名称已存在"
+    else
+      @commodity = Commodity.new(commodity_params)
+      @commodity.save!
+      flash[:notice] = "新增成功"
+    end
+    redirect_to commodities_path
+  end
+
   def import_commodity
     if !params[:file].present?
       flash[:alert] = "您还没有选择文件哦"
@@ -26,6 +38,12 @@ class CommoditiesController < ApplicationController
       end
     end
     redirect_to commodities_path
+  end
+
+  private
+
+  def commodity_params
+    params.require(:commodity).permit(:name, :commodity_code, :commodity_type_name, :commodity_type_code, :unit, :standart, :purchase_price, :selling_price)
   end
 
 end
