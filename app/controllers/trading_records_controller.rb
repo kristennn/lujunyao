000bbd896826ae5976f33eee1750cf888ec430f3
13949836783@163.com <13949836783@163.com>
2019-03-04 12:59:49 +0800

    录入交易记录bug解决

--- app/controllers/trading_records_controller.rb
+++ app/controllers/trading_records_controller.rb
@@ -20,26 +20,58 @@ class TradingRecordsController < ApplicationController
   end
 
   def create
+    @commodities = Commodity.where(id: params[:trading].keys)
+    @employees = Employee.all
+    error = {}
+    commodity_ids = []
+    employee_ids = []
     params[:trading].keys.each do |commodity_id|
       employee_id = params[:trading]["#{commodity_id}"]["employee_id"]
-      quantity = params[:trading]["#{commodity_id}"]["quantity"]
+      quantity = params[:trading]["#{commodity_id}"]["quantity"]     
       discount = params[:trading]["#{commodity_id}"]["discount"]
       price = (Commodity.find(commodity_id).selling_price) * discount.to_f
       total_amount = price * quantity.to_i
-      
-      #录入交易记录
-      TradingRecord.create(commodity_id: commodity_id, employee_id: employee_id, discount_price: price, quantity: quantity, discount: discount, total_amount: total_amount)    
-      #减少商品的库存
-      current_inventory = CommodityInventory.where(commodity_id: commodity_id).last.current_inventory - quantity.to_i
-      CommodityInventory.create(commodity_id: commodity_id, operate_type: "出库", quantity: quantity, current_inventory: current_inventory, operator: current_user.name, year: Time.now.year, month: Time.now.month)
-      #减少员工的易货币
+      commodity_inventory = CommodityInventory.where(commodity_id: commodity_id).last
       wage = Wage.where(employee_id: employee_id).last
-      net_virtual_money = wage.net_virtual_money + total_amount
-      remaining_virtual_money = wage.accumulative_virtual_money - total_amount
-      wage.update(net_virtual_money: net_virtual_money, accumulative_virtual_money: remaining_virtual_money)
+      if quantity.to_i > commodity_inventory.current_inventory
+        commodity_ids << commodity_id
+        error[1] = commodity_ids       
+      elsif total_amount > wage.accumulative_virtual_money
+        employee_ids << employee_id
+        error[2] = employee_ids       
+      end
     end
-    flash[:notice] = "录入交易成功"   
-    redirect_to choose_commodity_trading_records_path    
+    if error.present?
+      if error.keys.include?(1)
+        commodity_names = Commodity.where(id: error[1]).pluck(:name).uniq.compact
+        flash[:alert] = "您添加的#{commodity_names}交易数量超出库存，请检查"
+      else 
+        employee_names = Employee.where(id: error[2]).pluck(:name).uniq.compact
+        flash[:alert] = "本次交易的总金额超出了#{employee_names}的剩余易货币金额，请检查"
+      end
+      render :new
+    else
+      params[:trading].keys.each do |commodity_id|
+        employee_id = params[:trading]["#{commodity_id}"]["employee_id"]
+        quantity = params[:trading]["#{commodity_id}"]["quantity"]
+        discount = params[:trading]["#{commodity_id}"]["discount"]
+        price = (Commodity.find(commodity_id).selling_price) * discount.to_f
+        total_amount = price * quantity.to_i
+        commodity_inventory = CommodityInventory.where(commodity_id: commodity_id).last
+        wage = Wage.where(employee_id: employee_id).last
+        #录入交易记录
+        TradingRecord.create(commodity_id: commodity_id, employee_id: employee_id, discount_price: price, quantity: quantity, discount: discount, total_amount: total_amount)    
+        #减少商品的库存
+        current_inventory = commodity_inventory.current_inventory - quantity.to_i
+        CommodityInventory.create(commodity_id: commodity_id, operate_type: "出库", quantity: quantity, current_inventory: current_inventory, operator: current_user.name, year: Time.now.year, month: Time.now.month)
+        #减少员工的易货币   
+        net_virtual_money = wage.net_virtual_money + total_amount
+        remaining_virtual_money = wage.accumulative_virtual_money - total_amount
+        wage.update(net_virtual_money: net_virtual_money, accumulative_virtual_money: remaining_virtual_money)             
+      end
+      flash[:notice] = "录入交易成功" 
+      redirect_to trading_records_path
+    end          
   end
 
   def update
